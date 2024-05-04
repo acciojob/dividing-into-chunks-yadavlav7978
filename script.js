4d0f820629d4a370eb07121ec93dd9bee3d45378
Lav Yadav <yadavlav571@gmail.com>
2024-05-04 07:22:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
+	
     let sum = 0;
     let ans = [];
     let temp = [];
 
-    for (let i = 0; i < n; i++) {
+    for (let i = 0; i < arr.length; i++) {
         sum += arr[i];
         temp.push(arr[i]);
         if (sum > n) {
