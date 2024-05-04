3f2b9738ba77ebd224d381214a8326f4770549c1
Lav Yadav <yadavlav571@gmail.com>
2024-05-04 07:18:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
-  // Write your code here
+    let sum = 0;
+    let ans = [];
+    let temp = [];
+
+    for (let i = 0; i < n; i++) {
+        sum += arr[i];
+        temp.push(arr[i]);
+        if (sum > n) {
+            let last = temp.pop();
+            ans.push([...temp]); // Push a copy of temp into ans
+            temp = [];
+            temp.push(last);
+            sum = last;
+        }
+    }
+
+    ans.push(temp); // Push the remaining part of arr
+    return ans;
 };
 
 const n = prompt("Enter n: ");
